bb0ce046d7dfc87e8e8e0ac4a72a772f615c7656
s01d3t <p.chuntu@ngrsoftlab.ru>
2024-08-26 13:46:18 +0300

    omg

--- locators.py
+++ locators.py
@@ -6,6 +6,6 @@ class FirstPageLocators():
 
 
 class AdvPageLocators():
-    adv_title = (By.CLASS_NAME, 'styles_desktop_title__j0uNx')
+    adversitement_title = (By.CLASS_NAME, 'styles_desktop_title__j0uNx')
     agent_name = (By.CLASS_NAME, 'styles_agent__name__iexLd')
     whatsapp_button = (By.CSS_SELECTOR, 'a[data-testid="bottom-actions-whatsapp-button"]')
